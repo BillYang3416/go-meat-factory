777bf6ff199e365ba262bb88981502c6241e4e89
Bill Yang <billyang3416@gmail.com>
2024-03-12 23:43:06 +0800

    add meat struct and meatType enum

---
+++ internal/meat/meat.go
@@ -0,0 +1,31 @@
+package meat
+
+type MeatType int
+
+// 使用 Enum 定義肉的種類
+// 為何使用 iota:
+// 1. TypeSafety: 保證不會有其他的值被指派到 MeatType
+// 2. Efficiency: Sorting, Searching, Comparing
+const (
+	Beef MeatType = iota
+	Pork
+	Chicken
+)
+
+type Meat struct {
+	Type MeatType
+}
+
+// // 未來可依據 locale 擴充
+func (mt MeatType) String() string {
+	switch mt {
+	case Beef:
+		return "Beef"
+	case Pork:
+		return "Pork"
+	case Chicken:
+		return "Chicken"
+	default:
+		return "Unknown"
+	}
+}
