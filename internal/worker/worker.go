f6beb79de17139d686355b5849d80e1f8a78618a
Bill Yang <billyang3416@gmail.com>
2024-03-14 20:42:33 +0800

    fix text using code instead of sequences

--- internal/worker/worker.go
+++ internal/worker/worker.go
@@ -11,13 +11,15 @@ import (
 func StartWorker(ID int, meatCh <-chan meat.Meat, wg *sync.WaitGroup) {
 	defer wg.Done()
 
+	codes := []string{"A", "B", "C", "D", "E"}
+
 	for m := range meatCh {
 		// 使用員工編號和肉的編號來確認沒有人取到相同的肉
-		fmt.Printf("%d 號員工在 %v 取得編號為 %d 的%v\n", ID+1, time.Now().Format("2006-01-02 15:04:05"), m.ID, m.Type)
+		fmt.Printf("%v 在 %v 取得編號為 %d 的%v\n", codes[ID], time.Now().Format("2006-01-02 15:04:05"), m.ID, m.Type)
 
 		// 模擬處理肉的時間
 		time.Sleep(meat.ProcessingTime(m.Type))
 
-		fmt.Printf("%d 號員工在 %v 處理完編號為 %d 的%v\n", ID+1, time.Now().Format("2006-01-02 15:04:05"), m.ID, m.Type)
+		fmt.Printf("%v 在 %v 處理完編號為 %d 的%v\n", codes[ID], time.Now().Format("2006-01-02 15:04:05"), m.ID, m.Type)
 	}
 }
