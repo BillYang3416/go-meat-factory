f0769f8423953bfe84f04a8611bb9b827db05518
Bill Yang <billyang3416@gmail.com>
2024-03-14 19:57:21 +0800

    add comment which is comparing unbuffer channel with buffer channel

--- cmd/go-meat-factory/main.go
+++ cmd/go-meat-factory/main.go
@@ -24,6 +24,7 @@ func main() {
 	// 2. 先啟動 worker 準備從 receive channel 取出肉來處理，
 	// 	  並且處理中或處理後都需 print 出時間
 	var wg sync.WaitGroup
+	// 使用 buffered channel 增加吞吐量，會比 unbuffered channel 快
 	meatCh := make(chan meat.Meat, len(meats))
 	distributer.StartWorkers(numOfWorker, meatCh, &wg)
 
