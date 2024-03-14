e3bafb46a265be4759c8cc0206fdd9a7643ebd04
Bill Yang <billyang3416@gmail.com>
2024-03-14 20:48:27 +0800

    refactor comment

--- cmd/go-meat-factory/main.go
+++ cmd/go-meat-factory/main.go
@@ -21,16 +21,15 @@ func main() {
 	// 1. 進貨肉
 	meats := supplier.SupplyMeat(totalMeat, numOfBeef, numOfPork, numOfChicken)
 
-	// 2. 先啟動 worker 準備從 receive channel 取出肉來處理，
-	// 	  並且處理中或處理後都需 print 出時間
 	var wg sync.WaitGroup
 	// 使用 buffered channel 增加吞吐量，會比 unbuffered channel 快
 	meatCh := make(chan meat.Meat, len(meats))
+	// 2. 先啟動員工準備從 receive channel 取出肉來處理，並且處理中或處理完成後都需要印出時間
 	distributer.StartWorkers(numOfWorker, meatCh, &wg)
 
 	// 3. 將肉放到 send channel
 	distributer.DistributeToWorker(meatCh, meats)
 
-	// 4. 等待所有 worker 都處理完肉
+	// 4. 等待所有員工都處理完肉
 	wg.Wait()
 }
