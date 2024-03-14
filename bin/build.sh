0069996ce81be11aa0f41db64c679675ead200fd
Bill Yang <billyang3416@gmail.com>
2024-03-14 21:06:56 +0800

    update binary file and cmd

--- bin/build.sh
+++ bin/build.sh
@@ -1,3 +1,3 @@
-GOOS=linux GOARCH=amd64 go build -o linux/go-meat ../cmd/go-meat-factory/main.go
-GOOS=windows GOARCH=amd64 go build -o windows/go-meat.exe ../cmd/go-meat-factory/main.go
-GOOS=darwin GOARCH=amd64 go build -o darwin/go-meat ../cmd/go-meat-factory/main.go
+GOOS=linux GOARCH=amd64 go build -ldflags="-X 'main.Version=v1.0.0'" -o linux/go-meat ../cmd/go-meat-factory/main.go
+GOOS=windows GOARCH=amd64 go build -ldflags="-X 'main.Version=v1.0.0'" -o windows/go-meat.exe ../cmd/go-meat-factory/main.go
+GOOS=darwin GOARCH=amd64 go build -ldflags="-X 'main.Version=v1.0.0'" -o darwin/go-meat ../cmd/go-meat-factory/main.go
